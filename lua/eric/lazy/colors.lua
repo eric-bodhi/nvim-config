1303d705d10e36ca689e06318464cbdf85de59d8
Eric Floyd <erixefb@gmail.com>
2024-11-24 21:46:25 -0500

    used primeagen config

---
+++ lua/eric/lazy/colors.lua
@@ -0,0 +1,18 @@
+return {
+    "olimorris/onedarkpro.nvim",
+    styles = {
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "italic",
+        keywords = "bold,italic",
+        constants = "NONE",
+        functions = "italic",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "italic",
+        virtual_text = "NONE",
+    }
+}
